ec37289a575c765a78b2b3db7a403a0fcdf65cd5
weigang <ju@weigang>
2018-11-29 14:57:43 +0800

    aaa

--- src/test/java/com/gasq/bdp/logn/ApplicationTests.java
+++ src/test/java/com/gasq/bdp/logn/ApplicationTests.java
@@ -1,6 +1,5 @@
 package com.gasq.bdp.logn;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +23,7 @@ import com.gasq.bdp.logn.component.ImpalaOptions;
 import com.gasq.bdp.logn.mapper.TCompanyMapper;
 import com.gasq.bdp.logn.model.SystemUser;
 import com.gasq.bdp.logn.service.CommonService;
+import com.gasq.bdp.logn.utils.CommonUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -46,13 +46,13 @@ public class ApplicationTests {
 	
 	@Test
 	public void testRestTemplate() throws Exception {
-		final String url = user_info_url+"1/juwg";
+		final String url = user_info_url+"/juwg";
 		ResponseEntity<SystemUser> responseEntity = restTemplate.postForEntity(url, "juwg", SystemUser.class);
         System.out.println(responseEntity.getBody());
 	}
 	@Test
 	public void test1RestTemplate() throws Exception {
-		final String url = user_info_url;
+		final String url = "http://123.56.204.170:9999/systemuser/getCurrentUserInfo";
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
@@ -60,6 +60,33 @@ public class ApplicationTests {
 		params.add("password", "21232f297a57a5a743894a0e4a801fc3");
 		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
 		ResponseEntity<SystemUser> responseEntity = restTemplate.postForEntity(url,request, SystemUser.class);
+        System.out.println(responseEntity.getBody());
+	}
+	@Test
+	public void testUserSaveOrUpdate() throws Exception {
+		String url = "http://123.56.204.170:9999/systemuser/saveOrUpdate";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		TSysUser user = new TSysUser();
+		user.setUsername("test");
+		user.setAge(110);
+		user.setSex(1);
+		user.setStatus(99);
+		user.setIsvalid(false);
+		HttpEntity<String> entity = new HttpEntity<String>(CommonUtils.BeanToJSON(user), headers);
+		ResponseEntity<JSONObject> responseEntity = restTemplate.postForEntity(url, entity, JSONObject.class);
+        System.out.println(responseEntity.getBody());
+	}
+	
+	@Test
+	public void testUserDelete() throws Exception {
+		String url = "http://123.56.204.170:9999/systemuser/delete";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+		params.add("username", "juwg");
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
+		ResponseEntity<Boolean> responseEntity = restTemplate.postForEntity(url, request, Boolean.class);
         System.out.println(responseEntity.getBody());
 	}
 }
---
+++ src/test/java/com/gasq/bdp/logn/TSysUser.java
@@ -0,0 +1,166 @@
+package com.gasq.bdp.logn;
+
+import java.util.Date;
+
+public class TSysUser{
+
+	private Long id;
+	//用户编号
+    private String username;
+    //昵称
+    private String nickname;
+    //加盟密码MD5
+    private String password;
+    //门店id
+    private Integer companyid;
+    //职称
+    private String cardId;
+    //邮箱
+    private String email;
+    //电话号码
+    private String phonenum;
+    //年龄
+    private Integer age;
+    //性别
+    private Integer sex;
+    //业务系统用户ID
+    private String address;
+    //状态 0为正常 99为关闭
+    private Integer status;
+    //是否可用 0：不可用 1：可用
+    private Boolean isvalid;
+    //组织编码
+    private String groupName;
+    //创建时间
+    private Date createTime;
+
+    private Date updateTime;
+    
+	public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getCompanyid() {
+        return companyid;
+    }
+
+    public void setCompanyid(Integer companyid) {
+        this.companyid = companyid;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId == null ? null : cardId.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum == null ? null : phonenum.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Boolean getIsvalid() {
+        return isvalid;
+    }
+
+    public void setIsvalid(Boolean isvalid) {
+        this.isvalid = isvalid;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName == null ? null : groupName.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
